1a469269f756c954e428439c04a2b92178b3b26e
Rom1Slom <slomczynskiromain@yahoo.fr>
2025-08-31 11:32:12 +0200

    ajout debug

--- main/views.py
+++ main/views.py
@@ -4,6 +4,9 @@ from django.http import JsonResponse
 from .models import Formation, ContactFormation
 from .forms import ContactFormationForm
 from django.core.mail import send_mail
+import logging
+logger = logging.getLogger(__name__)
+
 
 def home(request):
     """Page d'accueil avec les 4 formations"""
@@ -40,6 +43,9 @@ def formation_detail(request, formation_id):
             contact = form.save(commit=False)
             contact.formation = formation
             contact.save()
+            # Avant send_mail
+            logger.warning(f"Envoi email à : {['contact@kompetans.fr']}")
+
             send_mail(
                         'Nouvelle demande de contact formation',
                         f'Nom : {contact.nom}\\nPrénom : {contact.prenom}\\nEmail : {contact.email}\\nMessage : {contact.message}',
@@ -48,20 +54,20 @@ def formation_detail(request, formation_id):
                         fail_silently=False,
                     )
             messages.success(request, 'Votre demande a été envoyée avec succès ! Nous vous recontacterons dans les plus brefs délais.')
+            # Après send_mail
             try:
-                result = send_mail(
-                    'Test Render',
-                    'Ceci est un test',
-                    'contact@kompetans.fr',
-                    ['tonadresse@gmail.com'],
-                    fail_silently=False,
-                )
-                print('Résultat envoi email:', result)
+                    result = send_mail(
+                        'Test Render',
+                        'Ceci est un test',
+                        'contact@kompetans.fr',
+                        ['slomczynskiromain@yahoo.fr'],
+                        fail_silently=False,
+                    )
+                    logger.warning('Résultat envoi email: %s', result)
             except Exception as e:
-                print('Erreur envoi email:', e)
-            return redirect('formation_detail_id', formation_id=formation.id)
-    else:
-        form = ContactFormationForm()
+                    logger.error('Erreur envoi email: %s', e)
+        else:
+                        form = ContactFormationForm()
     
     return render(request, 'main/formation_detail.html', {
         'formation': formation,
