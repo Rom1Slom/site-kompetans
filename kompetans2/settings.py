e1b3e98e8698efd2bc5b1c7cfcc64f0049f0bd61
Rom1Slom <slomczynskiromain@yahoo.fr>
2026-01-02 08:47:58 +0100

    CORRECTIF

--- kompetans2/settings.py
+++ kompetans2/settings.py
@@ -415,9 +415,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "ssl0.ovh.net"
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'contact@kompetans.fr'
 EMAIL_HOST_PASSWORD = 'Coline05be!'
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'contact@kompetans.fr'
 
 
