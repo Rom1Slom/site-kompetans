43af71a83b205813b6f57597cacd7b492175e352
Rom1Slom <slomczynskiromain@yahoo.fr>
2026-01-02 08:27:12 +0100

    email vers ovh

--- kompetans2/settings.py
+++ kompetans2/settings.py
@@ -412,12 +412,13 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.eu'
-EMAIL_PORT = 587
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "ssl0.ovh.net"
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'contact@kompetans.fr'
-EMAIL_HOST_PASSWORD = 'Emma01be!'
+EMAIL_HOST_PASSWORD = 'Coline05be!'
+EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'contact@kompetans.fr'
 
 
