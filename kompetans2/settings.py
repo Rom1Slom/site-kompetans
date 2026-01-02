32a03801ff0a2dfbfc600ae57814a1d801d8f890
Rom1Slom <slomczynskiromain@yahoo.fr>
2026-01-02 08:56:25 +0100

    pb ssl

--- kompetans2/settings.py
+++ kompetans2/settings.py
@@ -412,12 +412,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# Configuration email OVH (port 465/SSL)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "ssl0.ovh.net"
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'contact@kompetans.fr'
 EMAIL_HOST_PASSWORD = 'Coline05be!'
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'contact@kompetans.fr'
 
 
